26cd441acec8b2a8c963445c49d5ae7716af14d1
Sokwhan Huh <sokwhan@google.com>
2023-02-22 00:29:37 +0000

    Example handling both v1alpha1 and canonical protos

--- cel-test-app-mvn/src/main/java/dev/cel/testapp/App.java
+++ cel-test-app-mvn/src/main/java/dev/cel/testapp/App.java
@@ -1,18 +1,24 @@
 package dev.cel.testapp;
 
-import com.google.api.expr.v1alpha1.CheckedExpr;
-import com.google.api.expr.v1alpha1.Constant;
-import com.google.api.expr.v1alpha1.Expr;
-import com.google.api.expr.v1alpha1.SourceInfo;
-import dev.cel.common.CelAbstractSyntaxTree;
+import dev.cel.common.CelProtoAbstractSyntaxTree;
+import dev.cel.common.CelProtoV1Alpha1AbstractSyntaxTree;
 import dev.cel.common.types.CelTypes;
 import dev.cel.common.types.SimpleType;
+import dev.cel.expr.CheckedExpr;
+import dev.cel.expr.Constant;
+import dev.cel.expr.Expr;
+import dev.cel.expr.SourceInfo;
 import dev.cel.runtime.CelRuntime;
 import dev.cel.runtime.CelRuntimeFactory;
 
 /** Hello world! */
 public class App {
   public static void main(String[] args) throws Exception {
+    evaluateWithCanonicalCelProto();
+    evaluateWithDeprecatedV1Alpha1Proto();
+  }
+
+  private static void evaluateWithCanonicalCelProto() throws Exception {
     // Manually constructed checkedExpr.
     // In the real world, this should be produced by a compiler (go, cpp or in Java when made
     // available)
@@ -21,7 +27,10 @@ public class App {
             .putTypeMap(1, CelTypes.STRING)
             .setExpr(
                 Expr.newBuilder()
-                    .setConstExpr(Constant.newBuilder().setStringValue("Hello world!").build())
+                    .setConstExpr(
+                        Constant.newBuilder()
+                            .setStringValue("Hello world! (With canonical protos)")
+                            .build())
                     .setId(1)
                     .build())
             .setSourceInfo(
@@ -34,7 +43,44 @@ public class App {
 
     CelRuntime celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder().build();
     CelRuntime.Program program =
-        celRuntime.createProgram(CelAbstractSyntaxTree.fromCheckedExpr(checkedExpr));
+        celRuntime.createProgram(CelProtoAbstractSyntaxTree.fromCheckedExpr(checkedExpr).getAst());
+
+    Object evaluatedResult = program.eval();
+    System.out.println(evaluatedResult);
+    System.out.println(SimpleType.INT);
+  }
+
+  private static void evaluateWithDeprecatedV1Alpha1Proto() throws Exception {
+    // Manually constructed checkedExpr.
+    // In the real world, this should be produced by a compiler (go, cpp or in Java when made
+    // available)
+    com.google.api.expr.v1alpha1.CheckedExpr checkedExpr =
+        com.google.api.expr.v1alpha1.CheckedExpr.newBuilder()
+            .putTypeMap(
+                1,
+                com.google.api.expr.v1alpha1.Type.newBuilder()
+                    .setPrimitive(com.google.api.expr.v1alpha1.Type.PrimitiveType.STRING)
+                    .build())
+            .setExpr(
+                com.google.api.expr.v1alpha1.Expr.newBuilder()
+                    .setConstExpr(
+                        com.google.api.expr.v1alpha1.Constant.newBuilder()
+                            .setStringValue("Hello world! (With v1alpha1 protos)")
+                            .build())
+                    .setId(1)
+                    .build())
+            .setSourceInfo(
+                com.google.api.expr.v1alpha1.SourceInfo.newBuilder()
+                    .setLocation("<input>")
+                    .addLineOffsets(15)
+                    .putPositions(1, 0)
+                    .build())
+            .build();
+
+    CelRuntime celRuntime = CelRuntimeFactory.standardCelRuntimeBuilder().build();
+    CelRuntime.Program program =
+        celRuntime.createProgram(
+            CelProtoV1Alpha1AbstractSyntaxTree.fromCheckedExpr(checkedExpr).getAst());
 
     Object evaluatedResult = program.eval();
     System.out.println(evaluatedResult);
