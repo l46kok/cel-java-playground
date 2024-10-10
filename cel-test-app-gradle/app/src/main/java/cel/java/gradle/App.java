b26e4870cdecac23d95cc25e8e44f3b14b521caa
Sokwhan Huh <sokwhan@google.com>
2024-10-10 10:41:52 -0700

    Version update

--- cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
+++ cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
@@ -9,28 +9,34 @@ import dev.cel.parser.CelUnparserFactory;
 import dev.cel.runtime.CelEvaluationException;
 import dev.cel.runtime.CelRuntime;
 import dev.cel.runtime.CelRuntimeFactory;
+import dev.cel.optimizer.CelOptimizationException;
+import dev.cel.optimizer.CelOptimizerFactory;
+import dev.cel.optimizer.optimizers.ConstantFoldingOptimizer;
 import java.util.Map;
 
 public class App {
   // Construct the compilation and runtime environments.
-  // These instances are immutable and thus trivially thread-safe and amenable to caching.
-  private static final CelCompiler CEL_COMPILER =
-      CelCompilerFactory.standardCelCompilerBuilder().addVar("my_var", SimpleType.STRING).build();
-  private static final CelRuntime CEL_RUNTIME =
-      CelRuntimeFactory.standardCelRuntimeBuilder().build();
+  // These instances are immutable and thus trivially thread-safe and amenable to
+  // caching.
+  private static final CelCompiler CEL_COMPILER = CelCompilerFactory.standardCelCompilerBuilder()
+      .addVar("my_var", SimpleType.STRING).build();
+  private static final CelRuntime CEL_RUNTIME = CelRuntimeFactory.standardCelRuntimeBuilder().build();
 
   public static void main(String[] args) throws Exception {
-    // Compile the expression into an Abstract Syntax Tree.
-    CelAbstractSyntaxTree ast = CEL_COMPILER.compile("my_var + '!'").getAst();
+    System.out.println("hi");
+    CelAbstractSyntaxTree ast = CEL_COMPILER.compile("my_var in ['H','O']").getAst();
 
-    // Plan an executable program instance.
+    var celOptimizer = CelOptimizerFactory.standardCelOptimizerBuilder(CEL_COMPILER, CEL_RUNTIME)
+        .addAstOptimizers(
+
+            ConstantFoldingOptimizer.getInstance())
+        .build();
+
+    ast = celOptimizer.optimize(ast);
+
     CelRuntime.Program program = CEL_RUNTIME.createProgram(ast);
 
-    // Evaluate the program with an input variable.
-    String result = (String) program.eval(Map.of("my_var", "Hello World"));
-    System.out.println(result); // 'Hello World!'
-
-    // Unparse
-    System.out.println("Unparsed: " + CelUnparserFactory.newUnparser().unparse(ast));
+    Boolean result = (Boolean) program.eval(Map.of("my_var", "H"));
+    System.out.println(result);
   }
 }
