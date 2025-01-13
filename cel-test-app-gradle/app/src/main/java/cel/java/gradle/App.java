4072f5ba3a39db6e48e99fc38ab17c8ac6ace41e
Sokwhan Huh <sokwhan@google.com>
2025-01-13 15:24:03 -0800

    dev.cel.protobuf jar test

--- cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
+++ cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
@@ -1,39 +1,30 @@
 package cel.java.gradle;
 
 import dev.cel.common.CelAbstractSyntaxTree;
-import dev.cel.common.CelValidationException;
-import dev.cel.common.types.SimpleType;
-import dev.cel.compiler.CelCompiler;
-import dev.cel.compiler.CelCompilerFactory;
-import dev.cel.parser.CelUnparserFactory;
-import dev.cel.runtime.CelEvaluationException;
+import dev.cel.common.CelProtoAbstractSyntaxTree;
+import dev.cel.common.ast.CelConstant;
+import dev.cel.common.ast.CelExpr;
+import dev.cel.common.ast.CelExpr.CelList;
+import dev.cel.expr.CheckedExpr;
 import dev.cel.runtime.CelRuntime;
 import dev.cel.runtime.CelRuntimeFactory;
-import dev.cel.optimizer.CelOptimizationException;
-import dev.cel.optimizer.CelOptimizerFactory;
-import dev.cel.optimizer.optimizers.ConstantFoldingOptimizer;
+import java.io.FileInputStream;
 import java.util.Map;
 
 public class App {
-  // Construct the compilation and runtime environments.
-  // These instances are immutable and thus trivially thread-safe and amenable to
-  // caching.
-  private static final CelCompiler CEL_COMPILER = CelCompilerFactory.standardCelCompilerBuilder()
-      .addVar("my_var", SimpleType.STRING).build();
   private static final CelRuntime CEL_RUNTIME = CelRuntimeFactory.standardCelRuntimeBuilder().build();
 
   public static void main(String[] args) throws Exception {
     System.out.println("hi");
-    CelAbstractSyntaxTree ast = CEL_COMPILER.compile("my_var in ['H','O']").getAst();
+    // CelAbstractSyntaxTree ast = CEL_COMPILER.compile("my_var in ['H','O']").getAst();
+    CelExpr listExpr = CelExpr.newBuilder().setList(CelList.newBuilder().addElements(CelExpr.ofConstant(1L, CelConstant.ofValue(2L))).build()).build();
+    System.out.println(listExpr);
+    CheckedExpr checkedExpr;
+    try (FileInputStream fis = new FileInputStream("checkedExpr.bin")) {
+      checkedExpr = CheckedExpr.parseFrom(fis);
+    }
 
-    var celOptimizer = CelOptimizerFactory.standardCelOptimizerBuilder(CEL_COMPILER, CEL_RUNTIME)
-        .addAstOptimizers(
-
-            ConstantFoldingOptimizer.getInstance())
-        .build();
-
-    ast = celOptimizer.optimize(ast);
-
+    CelAbstractSyntaxTree ast = CelProtoAbstractSyntaxTree.fromCheckedExpr(checkedExpr).getAst();
     CelRuntime.Program program = CEL_RUNTIME.createProgram(ast);
 
     Boolean result = (Boolean) program.eval(Map.of("my_var", "H"));
