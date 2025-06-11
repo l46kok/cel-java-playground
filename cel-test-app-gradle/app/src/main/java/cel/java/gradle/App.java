1a80fecb50434a9c9be1949dd63dbd64f8f6ba6f
Sokwhan Huh <sokwhan@google.com>
2025-06-11 00:33:38 +0000

    Add policy compiler

--- cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
+++ cel-test-app-gradle/app/src/main/java/cel/java/gradle/App.java
@@ -12,6 +12,12 @@ import dev.cel.runtime.CelRuntimeFactory;
 import dev.cel.optimizer.CelOptimizationException;
 import dev.cel.optimizer.CelOptimizerFactory;
 import dev.cel.optimizer.optimizers.ConstantFoldingOptimizer;
+import dev.cel.policy.CelPolicy;
+import dev.cel.policy.CelPolicyParser;
+import dev.cel.policy.CelPolicyParserFactory;
+import dev.cel.policy.CelPolicyValidationException;
+import dev.cel.policy.CelPolicyCompiler;
+import dev.cel.policy.CelPolicyCompilerFactory;
 import java.util.Map;
 
 public class App {
@@ -21,6 +27,10 @@ public class App {
   private static final CelCompiler CEL_COMPILER = CelCompilerFactory.standardCelCompilerBuilder()
       .addVar("my_var", SimpleType.STRING).build();
   private static final CelRuntime CEL_RUNTIME = CelRuntimeFactory.standardCelRuntimeBuilder().build();
+
+  private static final CelPolicyParser CEL_POLICY_PARSER = CelPolicyParserFactory.newYamlParserBuilder().build();
+  private static final CelPolicyCompiler CEL_POLICY_COMPILER = CelPolicyCompilerFactory
+      .newPolicyCompiler(CEL_COMPILER, CEL_RUNTIME).build();
 
   public static void main(String[] args) throws Exception {
     System.out.println("hi");
@@ -38,5 +48,20 @@ public class App {
 
     Boolean result = (Boolean) program.eval(Map.of("my_var", "H"));
     System.out.println(result);
+
+    String policyOutput = executePolicy();
+    System.out.println("Policy output: " + policyOutput);
+  }
+
+  private static String executePolicy() throws Exception {
+    String policyDoc = """
+         name: "context_pb"
+         rule:
+           match:
+             - output: "'hello world'"
+        """;
+    CelPolicy celPolicy = CEL_POLICY_PARSER.parse(policyDoc);
+    CelAbstractSyntaxTree ast = CEL_POLICY_COMPILER.compile(celPolicy);
+    return (String) CEL_RUNTIME.createProgram(ast).eval();
   }
 }
